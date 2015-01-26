d299a8dc0b19326d7cffef432a66359620d2b8b0
Manuel Spierenburg <manuel.spierenburg@gmail.com>
2015-01-26 23:30:28 +0100

    Remove files from git

--- .zsh/aliases.zsh
+++
@@ -1,5 +0,0 @@
-# Colorize output, add file type indicator, and put sizes in human readable format
-alias ls='ls -GFh --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
