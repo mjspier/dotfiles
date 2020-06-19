eda1392562e4095e359f5f412615f8b25c5c7924
Manuel Spierenburg <mspierenburg@bol.com>
2020-06-19 11:46:21 +0200

    Fix

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,3 +1,3 @@
 function kctl_logs_first_pod {
-    kubectl get pods ${1} -o jsonpath='{.spec.containers[*].name}' | awk '{print$1}' | xargs kubectl logs ${1}
+    kubectl get ${1} -o jsonpath='{.spec.containers[*].name}' | awk '{print$1}' | xargs kubectl logs ${1}
 }
